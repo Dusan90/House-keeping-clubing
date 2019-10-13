5813fe1c455ca3fc58d7bd99311bac4b8d95696d
Dusan <ducati.duka@gmail.com>
2019-10-13 15:27:01 +0200

    changes

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -2,8 +2,10 @@ import React from 'react'
 
 function Error() {
     return (
-        <div>
-            error
+        <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center',height: '100vh'}}>
+            <h1>
+            Ups! Can't find that page...
+            </h1>
         </div>
     )
 }
